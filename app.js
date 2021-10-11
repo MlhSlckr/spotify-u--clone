54199e244b0a7dbf5f01b410795bdd11d040f9a6
MlhSlckr <selcukermelih@gmail.com>
2021-10-11 17:37:00 +0300

    a

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const right = document.querySelector(".right");
 const settings = document.querySelector(".settings");
 const arrow = document.querySelector(".arrow");
 const profile = document.querySelector(".profile");
+const plays = document.querySelectorAll(".play-btn");
+const cards = document.querySelectorAll(".card");
+const overlay = document.querySelector(".overlay");
+
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const item = e.target.parentElement;
@@ -19,4 +23,47 @@ right.addEventListener("click", () => {
   settings.classList.toggle("is-show");
   arrow.classList.toggle("rotate");
   profile.classList.toggle("background");
+});
+
+plays.forEach(function (btn) {
+  btn.addEventListener("click", (e) => {
+    console.log(e.currentTarget);
+  });
+});
+
+const codeArray = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+];
+
+function randomColor() {
+  let hexCode = "#";
+  for (let x = 0; x < 6; x++) {
+    hexCode += codeArray[Math.floor(Math.random() * codeArray.length)];
+  }
+  return hexCode;
+}
+
+cards.forEach((card) => {
+  card.addEventListener("mouseover", () => {
+    overlay.style.backgroundColor = randomColor();
+  });
+  card.addEventListener("mouseleave", () => {
+    overlay.style.backgroundColor = "";
+  });
 });
